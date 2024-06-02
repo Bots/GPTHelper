bc5503068e29940a8dad6ca939b22965996844c5
bots <botsone@gmail.com>
2024-06-02 02:09:41 -0600

    [chore](refactor): Got hotword detection working, stopped crashing

--- bipbot.py
+++ bipbot.py
@@ -1,4 +1,5 @@
-import asyncio
+import numpy as np
+import pyaudio
 from dotenv import load_dotenv
 import os
 import assemblyai as aai
@@ -13,12 +14,25 @@ from openai import AsyncOpenAI
 # Load environment variables from .env file
 load_dotenv()
 
+# Initialize audio stream
+FRAMES_PER_BUFFER = 24000
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+SAMPLE_RATE = 16000
+CHUNK = 24000
+WINDOW_LENGTH_SECS = 1.5
+SLIDING_WINDOW_SECS:float = 3/4
+p = pyaudio.PyAudio()
+
 # Initialize services clients
 elevenlabsClient = ElevenLabs()
 openaiClient = AsyncOpenAI(api_key = os.getenv('OPENAI_API_KEY'))
 aai.settings.api_key = os.getenv('ASSEMBLY_API_KEY')
 
 base_model = Resnet50_Arc_loss()
+
+out_audio = np.zeros(16000) #blank 1 sec audio
+print("Initial S",out_audio.shape)
 
 computer_hw = HotwordDetector(
     hotword = "computer",
@@ -28,15 +42,10 @@ computer_hw = HotwordDetector(
     relaxation_time = 2,
 )
 
-mic_stream = SimpleMicStream(
-    window_length_secs = 1.5,
-    sliding_window_secs = 0.75,    
-)
-
-def on_open(session_opened: aai.RealtimeSessionOpened):
+def transcriber_on_open(session_opened: aai.RealtimeSessionOpened):
     print("Session ID: ", session_opened.session_id)
 
-def on_data(transcript: aai.RealtimeTranscript):
+def transcriber_on_data(transcript: aai.RealtimeTranscript):
     if not transcript.text:
         return
 
@@ -45,32 +54,45 @@ def on_data(transcript: aai.RealtimeTranscript):
     else:
         print(transcript.text, end = "\r")
 
-def on_error(error: aai.RealtimeError):
+def transcriber_on_error(error: aai.RealtimeError):
     print("An error occurred: ", error)
 
-def on_close():
+def transcriber_on_close():
     print("Closing session")
 
-mic_stream.start_stream()
+transcriber = aai.RealtimeTranscriber(
+    sample_rate = 16000,
+    on_data = transcriber_on_data,
+    on_error = transcriber_on_error,
+    on_open = transcriber_on_open,
+    on_close = transcriber_on_close,
+)
+
+stream = p.open(
+    format = FORMAT,
+    channels = CHANNELS,
+    rate = SAMPLE_RATE,
+    input = True,
+    frames_per_buffer = FRAMES_PER_BUFFER
+)
 
 while True:
-    frame = mic_stream.getFrame()
-    result = computer_hw.scoreFrame(frame)
+    frame = (
+        np.frombuffer(stream.read(CHUNK, exception_on_overflow = False), dtype=np.int16)
+    )
+    out_audio = np.append(
+        out_audio[15999],
+        frame
+    )
+    print(out_audio.shape)
+    result = computer_hw.scoreFrame(inp_audio_frame = frame)
     if result == None:
         continue
     if(result["match"]):
         print("Wakeword uttered", result["confidence"])
 
-        transcriber = aai.RealtimeTranscriber(
-            sample_rate = 16_000,
-            on_data = on_data,
-            on_error = on_error,
-            on_open = on_open,
-            on_close = on_close,
-        )
-
         transcriber.connect()
 
-        transcriber.stream(mic_stream.getFrame())
+        transcriber.stream(stream)
 
         transcriber.close()
